236bf70f849141839220bccca2ee5be3428d9cfe
Jackson Magnabosco <jackson.magnabosco@globalsys.com.br>
2025-03-29 19:28:42 -0300

    Ultimos ajustes

--- api/routes/api.js
+++ api/routes/api.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../middlewares/auth'); 
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 // Middleware de autenticação para proteger rotas
 
 // Rota pública
